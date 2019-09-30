235bac30d88680e9083dbd30cc8cfc10c978f4d7
J-reyes132 <55826415+J-reyes132@users.noreply.github.com>
2019-09-29 22:40:34 -0400

    Add files via upload

---
+++ Proyecto N-capas1.3/Proyecto N-capas1.3/Form1.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txt_codigo = New System.Windows.Forms.TextBox()
+        Me.txt_nombre = New System.Windows.Forms.TextBox()
+        Me.txt_costo = New System.Windows.Forms.TextBox()
+        Me.txt_precio = New System.Windows.Forms.TextBox()
+        Me.txt_fechacreacion = New System.Windows.Forms.TextBox()
+        Me.txt_fechavencimiento = New System.Windows.Forms.TextBox()
+        Me.txt_categoria = New System.Windows.Forms.TextBox()
+        Me.txt_estado = New System.Windows.Forms.TextBox()
+        Me.Txt_detalle = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.dtgvproductos = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dtgvproductos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(534, 83)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Eliminar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(534, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Agregar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txt_codigo
+        '
+        Me.txt_codigo.Location = New System.Drawing.Point(142, 28)
+        Me.txt_codigo.Name = "txt_codigo"
+        Me.txt_codigo.Size = New System.Drawing.Size(100, 20)
+        Me.txt_codigo.TabIndex = 2
+        '
+        'txt_nombre
+        '
+        Me.txt_nombre.Location = New System.Drawing.Point(142, 54)
+        Me.txt_nombre.Name = "txt_nombre"
+        Me.txt_nombre.Size = New System.Drawing.Size(100, 20)
+        Me.txt_nombre.TabIndex = 3
+        '
+        'txt_costo
+        '
+        Me.txt_costo.Location = New System.Drawing.Point(142, 106)
+        Me.txt_costo.Name = "txt_costo"
+        Me.txt_costo.Size = New System.Drawing.Size(100, 20)
+        Me.txt_costo.TabIndex = 4
+        '
+        'txt_precio
+        '
+        Me.txt_precio.Location = New System.Drawing.Point(357, 28)
+        Me.txt_precio.Name = "txt_precio"
+        Me.txt_precio.Size = New System.Drawing.Size(100, 20)
+        Me.txt_precio.TabIndex = 5
+        '
+        'txt_fechacreacion
+        '
+        Me.txt_fechacreacion.Location = New System.Drawing.Point(357, 54)
+        Me.txt_fechacreacion.Name = "txt_fechacreacion"
+        Me.txt_fechacreacion.Size = New System.Drawing.Size(100, 20)
+        Me.txt_fechacreacion.TabIndex = 6
+        '
+        'txt_fechavencimiento
+        '
+        Me.txt_fechavencimiento.Location = New System.Drawing.Point(357, 80)
+        Me.txt_fechavencimiento.Name = "txt_fechavencimiento"
+        Me.txt_fechavencimiento.Size = New System.Drawing.Size(100, 20)
+        Me.txt_fechavencimiento.TabIndex = 7
+        '
+        'txt_categoria
+        '
+        Me.txt_categoria.Location = New System.Drawing.Point(357, 106)
+        Me.txt_categoria.Name = "txt_categoria"
+        Me.txt_categoria.Size = New System.Drawing.Size(100, 20)
+        Me.txt_categoria.TabIndex = 8
+        '
+        'txt_estado
+        '
+        Me.txt_estado.Location = New System.Drawing.Point(519, 28)
+        Me.txt_estado.Name = "txt_estado"
+        Me.txt_estado.Size = New System.Drawing.Size(100, 20)
+        Me.txt_estado.TabIndex = 9
+        '
+        'Txt_detalle
+        '
+        Me.Txt_detalle.Location = New System.Drawing.Point(142, 80)
+        Me.Txt_detalle.Name = "Txt_detalle"
+        Me.Txt_detalle.Size = New System.Drawing.Size(100, 20)
+        Me.Txt_detalle.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Codigo:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Nombre:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Detalle:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 104)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Costo:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(251, 31)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Precio"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(251, 54)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Fecha Creacion:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(251, 83)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(98, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Fecha Vencimiento"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(251, 109)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(55, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Categoria:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(470, 31)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(43, 13)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Estado:"
+        '
+        'dtgvproductos
+        '
+        Me.dtgvproductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtgvproductos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8, Me.Column9})
+        Me.dtgvproductos.Location = New System.Drawing.Point(26, 132)
+        Me.dtgvproductos.Name = "dtgvproductos"
+        Me.dtgvproductos.Size = New System.Drawing.Size(684, 185)
+        Me.dtgvproductos.TabIndex = 20
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Codigo"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Nombre"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Detalle"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Costo"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Precio"
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Fecha Creacion"
+        Me.Column6.Name = "Column6"
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Fecha vencimiento"
+        Me.Column7.Name = "Column7"
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "Categoria"
+        Me.Column8.Name = "Column8"
+        '
+        'Column9
+        '
+        Me.Column9.HeaderText = "Estado"
+        Me.Column9.Name = "Column9"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dtgvproductos)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Txt_detalle)
+        Me.Controls.Add(Me.txt_estado)
+        Me.Controls.Add(Me.txt_categoria)
+        Me.Controls.Add(Me.txt_fechavencimiento)
+        Me.Controls.Add(Me.txt_fechacreacion)
+        Me.Controls.Add(Me.txt_precio)
+        Me.Controls.Add(Me.txt_costo)
+        Me.Controls.Add(Me.txt_nombre)
+        Me.Controls.Add(Me.txt_codigo)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dtgvproductos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txt_codigo As TextBox
+    Friend WithEvents txt_nombre As TextBox
+    Friend WithEvents txt_costo As TextBox
+    Friend WithEvents txt_precio As TextBox
+    Friend WithEvents txt_fechacreacion As TextBox
+    Friend WithEvents txt_fechavencimiento As TextBox
+    Friend WithEvents txt_categoria As TextBox
+    Friend WithEvents txt_estado As TextBox
+    Friend WithEvents Txt_detalle As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents dtgvproductos As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As DataGridViewTextBoxColumn
+End Class
---
+++ Proyecto N-capas1.3/Proyecto N-capas1.3/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles txt_precio.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        DataGridViewRow fila = New DataGridViewRow();
+        fila.CreateCells(dtgvproductos);
+
+
+    End Sub
+End Class
